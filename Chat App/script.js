c6eece334a1787edc862c1733cd63a13cdc23c73
Deepika Koche <126641181+Deepika387@users.noreply.github.com>
2023-09-25 15:07:42 +0530

    Add files via upload

---
+++ Chat App/script.js
@@ -0,0 +1,20 @@
+const messageInput = document.getElementById("message-input");
+const sendButton = document.getElementById("send-button");
+const chatMessages = document.getElementById("chat-messages");
+
+sendButton.addEventListener("click", () => {
+    const message = messageInput.value.trim();
+    if (message !== "") {
+        appendMessage("You", message);
+        messageInput.value = "";
+    }
+});
+
+function appendMessage(sender, text) {
+    const messageDiv = document.createElement("div");
+    messageDiv.classList.add("message");
+    messageDiv.innerHTML = `<strong>${sender}:</strong> ${text}`;
+    chatMessages.appendChild(messageDiv);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
